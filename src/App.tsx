2cc02fc04736280973ef0c9b443ee346571308c8
Themion <themion@naver.com>
2024-10-25 20:48:44 +0900

    feat: test dev / build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Test from '~/components/Test';
+
 const App = () => {
   return (
     <>
       <h1 className="text-3xl text-center font-bold underline">Hello World</h1>
+      <Test />
     </>
   );
 };
