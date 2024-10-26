735d0e9fb0ba8f471703f5bd50a02959ef85cc18
Themion <themion@naver.com>
2024-10-26 18:30:52 +0900

    feat: use react-hook-form instead of zustand

--- src/components/ItemInput.tsx
+++ src/components/ItemInput.tsx
@@ -1,46 +1,28 @@
 import { X } from 'lucide-react';
-import { useRef, useState } from 'react';
+import { ComponentProps } from 'react';
 
 import { Button, Input } from './ui';
 
-const ItemInput = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [hasValue, setHasValue] = useState(false);
+type Props = Omit<ComponentProps<'input'>, 'type' | 'placeholder'>;
 
-  const handleInputChange = () => {
-    setHasValue(!!inputRef.current?.value);
-  };
-
-  const handleClearInput = () => {
-    if (!inputRef.current) return;
-    inputRef.current.value = '';
-    setHasValue(false);
-    inputRef.current.focus();
-  };
-
+const ItemInput = ({ ...props }: Props) => {
   return (
     <div className="flex w-full max-w-sm items-center space-x-2">
-      <div className="relative w-full">
-        <Input
-          type="text"
-          placeholder="Enter text..."
-          ref={inputRef}
-          onChange={handleInputChange}
-          className="pr-10"
-        />
-        {hasValue && (
-          <Button
-            type="button"
-            variant="ghost"
-            size="icon"
-            className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-            onClick={handleClearInput}
-            aria-label="Clear input"
-          >
-            <X className="h-4 w-4" />
-          </Button>
-        )}
-      </div>
+      <Input
+        type="text"
+        placeholder="Enter text..."
+        className="pr-10"
+        {...props}
+      />
+      <Button
+        type="button"
+        variant="ghost"
+        size="icon"
+        className=" h-full px-3 py-2 hover:bg-transparent"
+        aria-label="Clear input"
+      >
+        <X className="h-4 w-4" />
+      </Button>
     </div>
   );
 };
